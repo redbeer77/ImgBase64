b584a5ee26fd3a10a25057e23dec33522e4804cf
RedBeer77 <acastro1985@gmail.com>
2018-11-13 20:00:20 +0100

    Agregar archivos de proyecto.

---
+++ ImgBase64/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
